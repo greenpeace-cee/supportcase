ed1e20247bc2059bd49029ec1053929e7a8c8b5a
Patrick Figel <pfigel@greenpeace.org>
2022-03-03 13:56:05 +0100

    GP-25238 Add fullscreen mode to "View Original"

--- ang/manageCase.js
+++ ang/manageCase.js
@@ -696,6 +696,10 @@
                     $scope.switchToMode($scope.availableModes.view, activityId);
                 };
 
+                $scope.viewFullscreen = function(activityId) {
+                    window.open(CRM.url('civicrm/supportcase/view-original', {'id' : activityId}), '_blank').focus();
+                };
+
                 $scope.removeForwardAttachment = function(fileId, activityForwardData) {
                     activityForwardData['attachments'] = activityForwardData['attachments'].filter(function (item) {
                         return item['file_id'] !== fileId
@@ -1842,7 +1846,7 @@
                     var mainElement = CRM.$($element);
                     var iframeUrl = CRM.url('civicrm/supportcase/view-original', {'id' : $scope.activityId});
                     mainElement.empty();
-                    mainElement.append('<iframe class="com__iframe-origin" sandbox="allow-popups allow-popups-to-escape-sandboxswitchToMode" src="' + iframeUrl + '"></iframe>');
+                    mainElement.append('<iframe class="com__iframe-origin" sandbox="allow-popups allow-popups-to-escape-sandbox" src="' + iframeUrl + '"></iframe>');
                 };
             }
         };
