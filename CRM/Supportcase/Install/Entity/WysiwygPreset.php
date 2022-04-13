01b6319035296f426e2ac80336576a1d26b119c6
Patrick Figel <pfigel@greenpeace.org>
2022-04-13 13:56:24 +0200

    GP-26375 Fix CKEditor config setup for CiviCRM >= 5.40

--- CRM/Supportcase/Install/Entity/WysiwygPreset.php
+++ CRM/Supportcase/Install/Entity/WysiwygPreset.php
@@ -7,7 +7,17 @@ class CRM_Supportcase_Install_Entity_WysiwygPreset extends CRM_Supportcase_Insta
    */
   public function createAll() {
     $template = dirname(__DIR__) . '/../../../resources/crm-ckeditor-supportcase.js';
-    $file = Civi::paths()->getPath(CRM_Admin_Form_CKEditorConfig::CONFIG_FILEPATH . 'supportcase' . '.js');
+    $config_filepath = NULL;
+    if (class_exists('CRM_Admin_Form_CKEditorConfig')) {
+      $config_filepath = CRM_Admin_Form_CKEditorConfig::CONFIG_FILEPATH;
+    }
+    elseif (class_exists('CRM_Ckeditor4_Form_CKEditorConfig')) {
+      $config_filepath = CRM_Ckeditor4_Form_CKEditorConfig::CONFIG_FILEPATH;
+    }
+    else {
+      throw new Exception('Supportcase requires the ckeditor4 extension starting with CiviCRM 5.40');
+    }
+    $file = Civi::paths()->getPath($config_filepath . 'supportcase' . '.js');
     file_put_contents($file, file_get_contents($template));
   }
 
